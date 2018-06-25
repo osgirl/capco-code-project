b676dfc09d9f752816409fe81ec562197aa4785a
randallwhitlock <randall.whitlock@live.com>
2018-06-25 05:20:59 -0400

    updated path dep in file

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const icons = require('./assets/');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const icons = require("./assets/");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use(morgan('default'), helmet(), express.json(), cors());
-app.get('/api/icons', (req, res) => {
-  res.status(200).json({'data': icons})
+app.use(morgan("default"), helmet(), express.json(), cors());
+app.get("/api/icons", (req, res) => {
+  res.status(200).json({ data: icons });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-app.listen(port, () => console.log('Listening on port 8000...'))
+app.listen(port, () => console.log("Listening on port 8000..."));
