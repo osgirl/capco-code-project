49082485ea2e4e645a27bb8c21ce1f3b5b75597f
randallwhitlock <randall.whitlock@live.com>
2018-06-26 09:34:29 -0400

    several updates for functionality

---
+++ src/client/components/Rows/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Tile from "../Tile";
+import style from "./Rows.css";
+
+const Rows = props => {
+
+  const handleClick = (e, name, index,  cb) => {
+    e.preventDefault();
+    cb(e,name, index);
+  };
+
+  const { active } = props;
+
+  return (
+    <div className="rows">
+      {props.tiles.map(([name, data], index) => (
+        <Tile
+          className={"tile "}
+          name={name}
+          index={props.index}
+          isActive={active === name ? true : false}
+          handleClick={e => handleClick(e, name, index, props.clickHandler)}
+          src={data}
+          key={name}
+        />
+      ))}
+    </div>
+  );
+};
+export default Rows;
