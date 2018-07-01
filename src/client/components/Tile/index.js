d95b487d4832188280974ea31e4d84cc1c692de6
randallwhitlock <randall.whitlock@live.com>
2018-07-01 07:50:35 -0400

    one more update to style fixed transition after breaking it

--- src/client/components/Tile/index.js
+++ src/client/components/Tile/index.js
@@ -10,20 +10,23 @@ function rbg() {
   return `rgb(${rand()}, ${rand()}, ${rand()})`;
 }
 
-const Tile = ({ isActive, handleClick, src, name}) => {
-  const active = isActive ? "active" : "tile";
-  const color = isActive ? rbg() : "";
-  console.log({name, src, isActive, handleClick})
-  return (
-    <div
-      className={active}
-      onClick={e => handleClick(e, name)}
-      style={{ backgroundColor: color }}
-    >
-      <Svg alt={src[0]} src={src[1]} />
-    </div>
-  );
-};
+export default class Tile extends React.Component {
 
+  render() {
+    const { isActive, handleClick, src, name} = this.props;
 
-export default Tile;
+    const active = isActive ? "active" : "tile";
+    const color = isActive ? rbg() : "";
+    console.log({name, src, isActive, handleClick})
+  
+    return (
+      <div
+        className={active}
+        onClick={e => handleClick(e, name)}
+        style={{ backgroundColor: color }}
+      >
+        <Svg alt={src[0]} src={src[1]} />
+      </div>
+    );
+  };
+}
